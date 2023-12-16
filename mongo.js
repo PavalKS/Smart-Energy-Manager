8392282fe71bd29a5a8a764f26ec5e6b0bbed214
Paval <74084308+PavalKS@users.noreply.github.com>
2023-12-16 18:32:43 +0530

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://0.0.0.0:27017/loginsignupsmarthome")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    room:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    userType:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
