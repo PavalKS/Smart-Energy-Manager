72ab58c4d4fefb8907d4724743d8dca027c617ad
Paval <74084308+PavalKS@users.noreply.github.com>
2023-12-20 15:30:39 +0530

    Update Login.js

--- src/Login/client/Login.js
+++ src/Login/client/Login.js
@@ -5,18 +5,18 @@ import { useNavigate, Link } from "react-router-dom";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [userType, setUserType] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post("http://localhost:3001/Login", { email, password })
-      .then(response => {
+    axios
+      .post("http://localhost:3001/Login", { email, password })
+      .then((response) => {
         const message = response.data;
         switch (message) {
           case "Success":
-            navigate('/HomePage');
-            sessionStorage.setItem('loggedInUser', email);
+            navigate("/HomePage");
+            sessionStorage.setItem("loggedInUser", email);
             break;
           case "The password is incorrect":
           case "User doesn't exist":
@@ -27,28 +27,36 @@ function Login() {
             break;
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Login error:", error.message);
         window.alert("An error occurred during login");
       });
-  };
-
+  }
 
   return (
     <div className="login col-4">
-      <h1 className="display-3">Login</h1> 
-      <br></br>
+      <h1 className="display-3">Login</h1>
+      <br />
       <form onSubmit={handleSubmit}>
-        <input type="email" className="form-control" onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-        <br></br>
-        <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-        <br></br>
+        <input
+          type="email"
+          className="form-control"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br />
+        <input
+          type="password"
+          className="form-control"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <br />
         <input type="submit" className="form-control" value="Submit" />
       </form>
       <br />
       <p>
-        New User?     
-        <Link to="/Signup">     Signup</Link>
+        New User? <Link to="/Signup">Signup</Link>
       </p>
     </div>
   );
